7a1342a4c255bb138e5bd4bc6bb5f06e736c989b
qas275 <weishunlim93@hotmail.com>
2022-06-09 09:19:57 +0800

    first commit

---
+++ src/main/java/vttp2022/day3/workshop/App.java
@@ -0,0 +1,13 @@
+package vttp2022.day3.workshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
